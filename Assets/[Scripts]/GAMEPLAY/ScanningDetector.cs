a9433b13508f6b4e8c03b83aaf2ecce8ae6ba7a0
lucasdunster <60508486+lucasluvia@users.noreply.github.com>
2022-03-17 06:29:25 -0400

    Small ScaningDetector Modification
    Scanning detectors can now choose which direction they will scan in through an enum.

--- Assets/[Scripts]/GAMEPLAY/ScanningDetector.cs
+++ Assets/[Scripts]/GAMEPLAY/ScanningDetector.cs
@@ -2,7 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ScanDirection
+{
+    X_POS,
+    X_NEG,
+    Y_POS,
+    Y_NEG,
+    Z_POS,
+    Z_NEG
+}
+
+
 public class ScanningDetector : MonoBehaviour
 {
-    
+    public ScanDirection scanDirection;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetInteger("ScanDir", (int)scanDirection);
+    }
 }
